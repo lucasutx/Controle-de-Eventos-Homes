faca806ef1d9476271fc72e2986f65c7a459e0bd
Lucas <lucasxbitencourt77@gmail.com>
2024-12-28 07:25:20 -0300

    Design Form

--- Home's Pizzaria/Form1.Designer.cs
+++ Home's Pizzaria/Form1.Designer.cs
@@ -41,11 +41,11 @@
             label6 = new Label();
             txtValor = new TextBox();
             btnSoma = new Button();
-            label7 = new Label();
             label8 = new Label();
             label9 = new Label();
             dtpDataInicio = new DateTimePicker();
             dtpDataFim = new DateTimePicker();
+            label10 = new Label();
             SuspendLayout();
             // 
             // label1
@@ -86,7 +86,7 @@
             // 
             // txtNome
             // 
-            txtNome.Location = new Point(112, 110);
+            txtNome.Location = new Point(114, 107);
             txtNome.Name = "txtNome";
             txtNome.Size = new Size(167, 23);
             txtNome.TabIndex = 5;
@@ -125,14 +125,14 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.BackColor = SystemColors.Menu;
+            label5.BackColor = SystemColors.ButtonFace;
             label5.BorderStyle = BorderStyle.Fixed3D;
-            label5.Font = new Font("Yu Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label5.Location = new Point(244, 9);
+            label5.Font = new Font("Leelawadee", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(114, 61);
             label5.Name = "label5";
-            label5.Size = new Size(198, 27);
+            label5.Size = new Size(183, 25);
             label5.TabIndex = 10;
-            label5.Text = "Controle de Eventos";
+            label5.Text = "Cadastro de Eventos";
             // 
             // label6
             // 
@@ -153,7 +153,7 @@
             // 
             // btnSoma
             // 
-            btnSoma.Location = new Point(476, 223);
+            btnSoma.Location = new Point(486, 202);
             btnSoma.Name = "btnSoma";
             btnSoma.Size = new Size(69, 23);
             btnSoma.TabIndex = 13;
@@ -161,21 +161,10 @@
             btnSoma.UseVisualStyleBackColor = true;
             btnSoma.Click += btnSoma_Click;
             // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.BackColor = SystemColors.Control;
-            label7.BorderStyle = BorderStyle.Fixed3D;
-            label7.Location = new Point(470, 110);
-            label7.Name = "label7";
-            label7.Size = new Size(75, 17);
-            label7.TabIndex = 17;
-            label7.Text = "Defina Datas";
-            // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(379, 147);
+            label8.Location = new Point(395, 115);
             label8.Name = "label8";
             label8.Size = new Size(63, 15);
             label8.TabIndex = 18;
@@ -184,7 +173,7 @@
             // label9
             // 
             label9.AutoSize = true;
-            label9.Location = new Point(383, 191);
+            label9.Location = new Point(395, 166);
             label9.Name = "label9";
             label9.Size = new Size(54, 15);
             label9.TabIndex = 19;
@@ -194,7 +183,7 @@
             // 
             dtpDataInicio.CustomFormat = "";
             dtpDataInicio.Format = DateTimePickerFormat.Short;
-            dtpDataInicio.Location = new Point(448, 141);
+            dtpDataInicio.Location = new Point(464, 109);
             dtpDataInicio.Name = "dtpDataInicio";
             dtpDataInicio.Size = new Size(121, 23);
             dtpDataInicio.TabIndex = 20;
@@ -202,22 +191,34 @@
             // dtpDataFim
             // 
             dtpDataFim.Format = DateTimePickerFormat.Short;
-            dtpDataFim.Location = new Point(448, 185);
+            dtpDataFim.Location = new Point(464, 158);
             dtpDataFim.Name = "dtpDataFim";
             dtpDataFim.Size = new Size(121, 23);
             dtpDataFim.TabIndex = 21;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.ButtonFace;
+            label10.BorderStyle = BorderStyle.Fixed3D;
+            label10.Font = new Font("Leelawadee", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(418, 61);
+            label10.Name = "label10";
+            label10.Size = new Size(209, 25);
+            label10.TabIndex = 22;
+            label10.Text = "Valor Total por Periodo";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(696, 423);
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(755, 439);
+            Controls.Add(label10);
             Controls.Add(dtpDataFim);
             Controls.Add(dtpDataInicio);
             Controls.Add(label9);
             Controls.Add(label8);
-            Controls.Add(label7);
             Controls.Add(btnSoma);
             Controls.Add(txtValor);
             Controls.Add(label6);
@@ -252,10 +253,10 @@
         private Label label6;
         private TextBox txtValor;
         private Button btnSoma;
-        private Label label7;
         private Label label8;
         private Label label9;
         private DateTimePicker dtpDataInicio;
         private DateTimePicker dtpDataFim;
+        private Label label10;
     }
 }
